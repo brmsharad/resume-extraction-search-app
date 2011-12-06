40f895567670e9fb27c423f7632881b7ba803f50
rahul.simhadri@gmail.com <rahul.simhadri@gmail.com@2a585b50-4b60-28ad-25ae-1fc29b0d3364>
2011-12-06 00:19:53 +0000

    simple docx file completely parsed

--- trunk/resume_exctraction/parseDocx.php
+++ trunk/resume_exctraction/parseDocx.php
@@ -1,13 +1,83 @@
 <?php
 
-$xml = simplexml_load_file('resume.html');
-var_dump($xml->p);
-$p = $xml->p;
-foreach($p as $para)
-{
-    echo $para;
+$doc = new DOMDocument();
+$xml = $doc->load('1/word/document.xml');
+$xpath = new DOMXPath($doc);
+$xpath->registerNamespace('w', "http://schemas.openxmlformats.org/wordprocessingml/2006/main");
+
+
+$name_node = $xpath->query("//w:body/w:p/w:pPr/w:pStyle[@w:val='Name']");
+$name = $name_node->item(0)->parentNode->parentNode->nodeValue;
+echo $name;
+
+$address_node = $xpath->query("//w:body/w:p/w:pPr/w:pStyle[@w:val='Address']");
+$street_address = $address_node->item(0)->parentNode->parentNode->nodeValue;
+echo "street address" . $street_address;
+$city_zip_state = $address_node->item(1)->parentNode->parentNode->nodeValue;
+echo "street address" . $city_zip_state;
+$phone = $address_node->item(2)->parentNode->parentNode->nodeValue;
+echo "street address" . $phone;
+$email = $address_node->item(3)->parentNode->parentNode->nodeValue;
+echo "street address" . $email;
+
+$headings = $xpath->query("//w:body/w:p/w:pPr/w:pStyle[@w:val='BusinessNameDates']");
+foreach ($headings as $heading) {
+    $company_name_dates_node = $heading->parentNode->parentNode;
+    $company_name_dates = $company_name_dates_node->nodeValue;
+    $job_title_dicription_node = $company_name_dates_node->nextSibling->nodeValue;
+    echo $job_title_dicription_node;
 }
-        
+
+$resume_headings_list = $xpath->query("//w:body/w:p/w:pPr/w:pStyle[@w:val='ResumeHeadings']");
+
+foreach($resume_headings_list as $heading)
+{
+   $heading_node = $heading->parentNode->parentNode;
+   if($heading_node->nodeValue === 'Education')
+   {
+       $next_node = $heading_node->nextSibling;
+       echo $next_node->nodeValue;
+       
+       $second_next_node = $next_node->nextSibling;
+       echo $second_next_node->nodeValue;
+   }
+   
+   if($heading_node->nodeValue === 'Achievements/Awards')
+   {
+       $next_node = $heading_node->nextSibling;
+       $next_node_style = $next_node->firstChild->firstChild->getAttributeNS("http://schemas.openxmlformats.org/wordprocessingml/2006/main","val");
+       echo $next_node_style;
+       while($next_node_style === 'Overviewbullets' && $next_node_style!= 'ResumeHeading')
+       {
+           echo $next_node->nodeValue;
+           
+           $next_node = $next_node->nextSibling;
+       $next_node_style = $next_node->firstChild->firstChild->getAttributeNS("http://schemas.openxmlformats.org/wordprocessingml/2006/main","val");
+       }
+   }
+   
+   if($heading_node->nodeValue === 'Skills')
+   {
+       $next_node = $heading_node->nextSibling;
+       $next_node_style = $next_node->firstChild->firstChild->getAttributeNS("http://schemas.openxmlformats.org/wordprocessingml/2006/main","val");
+       echo $next_node_style;
+       while($next_node_style === 'Overviewbullets' && $next_node_style!= 'ResumeHeading')
+       {
+           echo $next_node->nodeValue;
+           
+           $next_node = $next_node->nextSibling;
+         
+           if($next_node->nodeName === 'w:p'){
+       $next_node_style = $next_node->firstChild->firstChild->getAttributeNS("http://schemas.openxmlformats.org/wordprocessingml/2006/main","val");
+                  }
+ else {
+    $next_node_style= null;
+}
+       }
+   }
+   
+}
+
 
 
 ?>
