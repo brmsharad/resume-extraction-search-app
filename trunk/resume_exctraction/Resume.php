e5c9b6109385fa4d76c43ab6b8990b8ff46c6037
rahul.simhadri@gmail.com <rahul.simhadri@gmail.com@2a585b50-4b60-28ad-25ae-1fc29b0d3364>
2011-12-10 04:37:28 +0000

    resume class

--- trunk/resume_exctraction/Resume.php
+++ trunk/resume_exctraction/Resume.php
@@ -1,9 +1,19 @@
 <?php
-
 class Resume {
- string 
- 
- 
-}
+ $uid = 0;
+ $first_name = '';
+ $last_name = '';
+ $email = '';
+$address= '';
+$city = '';
+$state = '';
+$zip = 0;
+$experiance = array();
+$school = array();
+$awards = array();
+$skills = array();
+};
 
+
+$skills[][]= '';
 ?>
