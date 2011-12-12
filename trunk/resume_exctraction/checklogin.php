8f2256c3c4c2f276852136c4bc239c5728814a7c
rahul.simhadri@gmail.com <rahul.simhadri@gmail.com@2a585b50-4b60-28ad-25ae-1fc29b0d3364>
2011-12-12 05:01:59 +0000

    to database

--- trunk/resume_exctraction/checklogin.php
+++ trunk/resume_exctraction/checklogin.php
@@ -1,9 +1,6 @@
 <?php
-$host="localhost"; // Host name
-$username="root"; // Mysql username
-$password=""; // Mysql password
-$db_name="resume_extraction"; // Database name
 
+require_once('include/database.php');
 $myusername=$_POST['username'];
 $mypassword=$_POST['passwd'];
 echo $mypassword;
@@ -13,20 +10,23 @@ $mypassword = stripslashes($mypassword);
 $myusername = mysql_real_escape_string($myusername);
 $mypassword = mysql_real_escape_string($mypassword);
 $mypassword = md5($mypassword);
-mysql_connect("$host", "$username", "$password")or die("cannot connect");
-mysql_select_db("$db_name")or die("cannot select DB");
+
 
 $sql="SELECT * FROM users WHERE name='$myusername' and pass='$mypassword'";
 $result=mysql_query($sql);
-var_dump($result);
+
 // Mysql_num_row is counting table row
 $count = mysql_num_rows($result);
 // If result matched $myusername and $mypassword, table row must be 1 row
 
 if($count==1){
 // Register $myusername, $mypassword and redirect to file "login_success.php"
+   $uid = mysql_fetch_object($result);
     session_start();
+
+$_SESSION['user_name']= $myusername;
 $_SESSION['user_type']="authenticated";
+$_SESSION['uid'] = $uid;
 //session_register("mypassword");
 header("location:test.php");
 }
--- trunk/resume_exctraction/parseDocx.php
+++ trunk/resume_exctraction/parseDocx.php
@@ -2,7 +2,18 @@
 class parseDocx
 {
     
-    
+    public $firstname = '';
+    public $lastname = '';
+    public $address = '';
+    public $city = '';
+    public $state = '';
+    public $zip = 0;
+    public $phone = '';
+    public $email = '';
+    public $company = array();
+    public $school = array();
+    public $awards = array();
+    public $skills = array();
     
     
      function __construct($xmlFile) {
@@ -22,24 +33,40 @@ $xpath->registerNamespace('w', "http://schemas.openxmlformats.org/wordprocessing
 
 $name_node = $xpath->query("//w:body/w:p/w:pPr/w:pStyle[@w:val='Name']");
 $name = $name_node->item(0)->parentNode->parentNode->nodeValue;
-echo $name;
+$name_array = explode(' ',$name);
+$this->firstname = $name_array[0];
+$this->lastname = $name_array[1];
+
 
 $address_node = $xpath->query("//w:body/w:p/w:pPr/w:pStyle[@w:val='Address']");
 $street_address = $address_node->item(0)->parentNode->parentNode->nodeValue;
-echo "street address" . $street_address;
+$this->address = $street_address;
+//echo "street address" . $street_address;
 $city_zip_state = $address_node->item(1)->parentNode->parentNode->nodeValue;
-echo "street address" . $city_zip_state;
+$city_zip_state_array = explode(',',$city_zip_state);
+$this->city = $city_zip_state_array[0];
+$this->zip = $city_zip_state_array[1];
+$this->state = $city_zip_state_array[2];
+//echo "street address" . $city_zip_state;
 $phone = $address_node->item(2)->parentNode->parentNode->nodeValue;
-echo "street address" . $phone;
+$this->phone = $phone;
+//echo "street address" . $phone;
 $email = $address_node->item(3)->parentNode->parentNode->nodeValue;
-echo "street address" . $email;
+$this->email = $email;
+//echo "street address" . $email;
 
 $headings = $xpath->query("//w:body/w:p/w:pPr/w:pStyle[@w:val='BusinessNameDates']");
 foreach ($headings as $heading) {
+
     $company_name_dates_node = $heading->parentNode->parentNode;
     $company_name_dates = $company_name_dates_node->nodeValue;
-    $job_title_dicription_node = $company_name_dates_node->nextSibling->nodeValue;
-    echo $job_title_dicription_node;
+    $company_name_dates_array = explode(",", $company_name_dates);
+     $this->company[]['company_name']= $company_name_dates_array[0];
+     $this->company[]['years'] = $company_name_dates_array[1];
+    $job_title_dicription = $company_name_dates_node->nextSibling->nodeValue;
+$job_title_dicription_array = explode(',',$job_title_dicription);
+    $this->company[]['job_title'] = $job_title_dicription_array[0];
+     $this->company[]['job_description'] = $job_title_dicription_array[1];
 }
 
 $resume_headings_list = $xpath->query("//w:body/w:p/w:pPr/w:pStyle[@w:val='ResumeHeadings']");
@@ -54,10 +81,12 @@ foreach($resume_headings_list as $heading)
    {
 
        $next_node = $heading_node->nextSibling;
-       echo $next_node->nodeValue;
-       
+       $degreeSchool =  $next_node->nodeValue;
+       $degreeSchoolArray = explode(",",$degreeSchool);
+       $this->school['degree'] = $degreeSchoolArray[0];
+       $this->school['school'] = $degreeSchoolArray[1];
        $second_next_node = $next_node->nextSibling;
-       echo $second_next_node->nodeValue;
+       $this->school['dates'] =  $second_next_node->nodeValue;
    }
    
    if(trim($heading_node->nodeValue) === 'Achievements/Awards')
@@ -68,7 +97,7 @@ foreach($resume_headings_list as $heading)
        echo $next_node_style;
        while($next_node_style === 'Overviewbullets' && $next_node_style!= 'ResumeHeading')
        {
-           echo $next_node->nodeValue;
+           $this->awards[] = $next_node->nodeValue;
            
            $next_node = $next_node->nextSibling;
        $next_node_style = $next_node->firstChild->firstChild->getAttributeNS("http://schemas.openxmlformats.org/wordprocessingml/2006/main","val");
@@ -82,7 +111,7 @@ foreach($resume_headings_list as $heading)
        echo $next_node_style;
        while($next_node_style === 'Overviewbullets' && $next_node_style!= 'ResumeHeading')
        {
-           echo $next_node->nodeValue;
+           $this->skills[]= $next_node->nodeValue;
            
            $next_node = $next_node->nextSibling;
          
